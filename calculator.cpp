d67ab410cd28ab8d8b7a81320dab363c1b56eecd
ashutosh432gt <130464258+ashutosh432gt@users.noreply.github.com>
2023-04-15 10:52:41 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter the value of a:-");
+	scanf("%d",&a);
+	printf("Enter the value of b:-");
+	scanf("%d",&b);
+	printf("The addition is %d",a+b);
+	printf("\nThe substraction is %d",a-b);
+	printf("\nThe multiplication is %d",a*b);
+	printf("\nThe division is %d",a/b);
+	
+}
