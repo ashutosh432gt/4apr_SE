9939daf9999aa75534a2618e14b64ab4aa6614ce
ashutosh432gt <130464258+ashutosh432gt@users.noreply.github.com>
2023-06-01 18:07:32 +0530

    Add files via upload

---
+++ C++ & OOPS(ASSIGNMENT)/Q26_READ_&_WRITE_DATA_USING_CLASS.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+class kunal
+{   
+	public:
+		string data;
+		void sa()
+		{
+			ofstream file1;
+			file1.open("test.txt");
+			
+			cout<<"Enter data:- ";
+			cin>>data;
+		}
+		void qw()
+		{
+			cout<<data;
+		}
+};
+main()
+{
+	kunal s;
+	s.sa();
+	s.qw();
+}
